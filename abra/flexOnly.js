2d5a8ac6c720b164822cfe592e200f3bf013962f
davidcurtiscole <57600940+davidcurtiscole@users.noreply.github.com>
2024-03-21 13:19:40 -0500

    Create flexOnly.js

---
+++ abra/flexOnly.js
@@ -0,0 +1,44 @@
+var flexOnly = window.prompt('enter what you copied');
+var str = flexOnly;
+var amount = str.split('\n').length;
+var flexSplit = flexOnly.split("\n");
+console.log(amount.length);
+
+for (var i = 0; i < amount; i++) {
+
+    var rows = document.querySelectorAll("table tbody tr");
+
+    [].forEach.call(rows, function(tr) {
+        var td = tr.querySelector("td:nth-child(4)");
+        console.log(td ? td.textContent.trim() : "N/A");
+        if (td && td.textContent.trim() === flexSplit[i]) {
+            var notePad = tr.querySelector("td:nth-child(11)");
+            if (notePad) {
+                var overlay = notePad.querySelector(".dataTableEditTask");
+                if (overlay) {
+                    if (overlayIsFullyLoaded(overlay)) {
+                        overlay.dispatchEvent(new Event("click", {
+                            bubbles: true
+                        }));
+                    }
+                }
+            }
+        }
+    });
+
+    function overlayIsFullyLoaded(overlay) {
+        return true;
+    };
+
+
+    setTimeout(function() {
+        if (document.getElementById('excludeFromDirected').checked === false) {
+            document.getElementById('excludeFromDirected').click();
+            setTimeout(function() {
+                document.getElementsByClassName('ui-dialog-titlebar-close ui-corner-all')[1].click();
+            }, 1500);
+        } else {
+            document.getElementsByClassName('ui-dialog-titlebar-close ui-corner-all')[1].click();
+        }
+    }, 1500);
+};
